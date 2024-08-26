e2f2441c8c3a4ab9694b8d76fd07be8ffb65ce0f
stiven-hidri <hidristiven@gmail.com>
2024-08-26 17:23:36 +0200

    fix vertical synthetic genearation

--- src/generate_synthetic_images.py
+++ src/generate_synthetic_images.py
@@ -15,10 +15,10 @@ PATH_TO_SYNTHETIC_DEFECTS_HARMONIZED = os.path.join(ROOT_DIR, 'data', 'Synthetic
 
 #borders of the frame. X and Y coordinates (origin is TOP LEFT). Taken using paint (while moving the cursor gives x and y coordinates)
 FRAME_BORDERS = {
-    'TL': (240, 120),
-    'TR': (1275, 130),
-    'BL': (170, 885),
-    'BR': (1275, 880)
+    'L': 240,
+    'R': 1260,
+    'T': 130,
+    'B': 880
 }
 
 def delete_old_files():
@@ -46,9 +46,9 @@ def get_pos_topleft(image):
     top_left = true_points.min(axis=0)
     return top_left
 
-def main(samples_number_per_defect = 10, probability_few_defects = .8):
+def main(samples_to_generate_per_defect = 10, probability_few_defects = .8):
     # probability of having few defects against having many defects (few and many defined next)
-    masks_paths = [ os.path.join(PATH_TO_MASKS, mask_folder, mask_filename) for mask_folder in os.listdir(PATH_TO_MASKS) if os.path.isdir(os.path.join(PATH_TO_MASKS, mask_folder)) for mask_filename in os.listdir(os.path.join(PATH_TO_MASKS, mask_folder)) if (mask_filename.endswith('.jpg') or mask_filename.endswith('.png')) and '_PD_' in mask_filename and not(int(mask_folder.split('Image')[-1]) < 50 and 'Spattering' in mask_filename ) ]
+    masks_paths = [ os.path.join(PATH_TO_MASKS, mask_folder, mask_filename) for mask_folder in os.listdir(PATH_TO_MASKS) if os.path.isdir(os.path.join(PATH_TO_MASKS, mask_folder)) for mask_filename in os.listdir(os.path.join(PATH_TO_MASKS, mask_folder)) if (mask_filename.endswith('.jpg') or mask_filename.endswith('.png')) and '_PD_' in mask_filename ]
     nodefects_filenames = [f for f in os.listdir(PATH_TO_NODEFECTS) if f.endswith('.jpg') or f.endswith('.png')]
     defect_types = list(set([f.split('_')[3].split('.')[0] for f in masks_paths]))
 
@@ -61,86 +61,101 @@ def main(samples_number_per_defect = 10, probability_few_defects = .8):
     
     print('Generating synthetic images...')
     
-    print(f'0/{samples_number_per_defect*len(defect_types)}', end='\r')
+    print(f'0/{samples_to_generate_per_defect*len(defect_types)}', end='\r')
     
-    for i_dt, chosen_defect_type in enumerate(defect_types):
-        for i_sndp in range(samples_number_per_defect):
+    for i_chosen_defect, chosen_defect_type in enumerate(defect_types):
+        for i_samples_to_generate_per_defect in range(samples_to_generate_per_defect):
             nodefects_filename = random.choice(nodefects_filenames)
             path_to_nodefect_image = os.path.join(PATH_TO_NODEFECTS, nodefects_filename)
-            nodefect_image = Image.open(path_to_nodefect_image).convert('RGB')
             nodefect_name = nodefects_filename.split('.')[0]
             
-            nodefect_image_current = nodefect_image.copy()
-            number_of_defects = choose_how_many_defects()
-            current_mask = None
+            nodefect_image = Image.open(path_to_nodefect_image).convert('RGB')
             
-            for def_i in range(number_of_defects):
-                if def_i == 0:
-                    #current mask store a union of all masks of the chosen defects, It will be used to check if any defect overlaps
-                    current_mask = np.zeros((nodefect_image.size[1], nodefect_image.size[0]), dtype=np.uint8)
+            number_of_defects = choose_how_many_defects() if 'Vertical' not in chosen_defect_type else random.randint(1,2)
+            
+            current_whole_mask = None
+            
+            for i_number_of_defects in range(number_of_defects):
+                if i_number_of_defects == 0:
+                    current_whole_mask = np.zeros((nodefect_image.size[1], nodefect_image.size[0]), dtype=np.uint8)
                 
-                #choose which defect will be used (so choose the file of its mask)
                 defect_mask_path = get_defect_mask_path_by_type(chosen_defect_type)
-                #get in which image is this defect placed
                 defect_name =  os.path.basename(defect_mask_path).split('_')[0]
-                
-                #read the defect image
                 path_to_defect_image = os.path.join(PATH_TO_DEFECTS, defect_name, f'{defect_name}.jpg')
-                defect_image = Image.open(path_to_defect_image).convert('RGB')
                 
-                # read the defect mask
+                defect_image = Image.open(path_to_defect_image).convert('RGB')
                 defect_mask = Image.open(defect_mask_path).convert('L')
+                
+                if os.path.basename(defect_mask_path) == 'Image43_PD_05_Vertical.png':
+                    defect_image = defect_image.rotate(-3)
+                    defect_mask = defect_mask.rotate(-3)
+                    original_side_is_right = True
+                    
+                if os.path.basename(defect_mask_path) == 'Image34_PD_02_Vertical.png':
+                    defect_image = defect_image.rotate(.5)
+                    defect_mask = defect_mask.rotate(.5)
+                    original_side_is_right = False
+                    
                 #get the top left coordinates of the mask
                 original_topleft_pos = get_pos_topleft(np.array(defect_mask))
-                #the a numpy copy of the mask (used for checking any overlap)
                 proto_cropped_mask = crop_image(np.array(defect_mask))
                 
-                keep_trying = True
-                count_tryies = 0
-                
-                while keep_trying == True and count_tryies < 10:
-                    #choose a differebt top left position (translation)
-                    try:
-                        x_start = random.randint(max(FRAME_BORDERS['TL'][0], FRAME_BORDERS['BL'][0]), min(FRAME_BORDERS['TR'][0], FRAME_BORDERS['BR'][0])-proto_cropped_mask.shape[1])
-                        y_start = random.randint(max(FRAME_BORDERS['TL'][1], FRAME_BORDERS['TR'][1]), min(FRAME_BORDERS['BL'][1], FRAME_BORDERS['BR'][1])-proto_cropped_mask.shape[0])
-                    except:
-                        count_tryies+=1
-                        continue
+                count_tries = 0
+                while count_tries < 10:
+
+                    x_start = random.randint(FRAME_BORDERS['L'], FRAME_BORDERS['R'] - proto_cropped_mask.shape[1])
+                    y_start = random.randint(FRAME_BORDERS['T'], FRAME_BORDERS['B'] - proto_cropped_mask.shape[0])
                     
-                    #we will use this in order to move the defect to the chosen position e check then if there are any overlapping with the defects chosen so far
                     test_mask = np.zeros((nodefect_image.size[1], nodefect_image.size[0]), dtype=np.uint8)
                     
                     x_end = x_start + proto_cropped_mask.shape[1]
                     y_end = y_start + proto_cropped_mask.shape[0]
                     
-                    test_mask[y_start:y_end, x_start:x_end] = proto_cropped_mask
-                    
-                    if not np.any(test_mask*current_mask) or def_i==0:
-                        keep_trying = False
-                        current_mask = (current_mask > 127).astype(np.uint8) * 255
-                        test_mask = (test_mask > 127).astype(np.uint8) * 255
-                        current_mask = np.logical_or(current_mask, test_mask).astype(np.uint8) * 255
-                        
-                    count_tryies+=1
+                    current_side_is_right = False if (x_start+x_end)//2 < 760 else True
                 
-                #past the cropped defect in the original image
-                nodefect_image_current.paste(defect_image, (x_start - original_topleft_pos[1], y_start - original_topleft_pos[0]), defect_mask)
+                    if original_side_is_right != current_side_is_right and 'Vertical' in chosen_defect_type:
+                        defect_mask = Image.fromarray(np.fliplr(defect_mask))
+                        defect_image = Image.fromarray(np.fliplr(defect_image))
+                        
+                        if current_side_is_right:
+                            x_start = x_start - 20
+                        else:
+                            x_start = x_start + 20
+                        
+                        original_topleft_pos = get_pos_topleft(np.array(defect_mask))
+                        flipped_mask = crop_image(np.array(defect_mask))
+                        x_end = x_start + flipped_mask.shape[1]
+                        y_end = y_start + flipped_mask.shape[0]
+                        
+                        test_mask[y_start:y_end, x_start:x_end] = flipped_mask
+                    else:     
+                        test_mask[y_start:y_end, x_start:x_end] = proto_cropped_mask
+                    
+                    if not np.any(test_mask*current_whole_mask) or i_number_of_defects==0:
+                        current_whole_mask = (current_whole_mask > 127).astype(np.uint8) * 255
+                        test_mask = (test_mask > 127).astype(np.uint8) * 255
+                        current_whole_mask = np.logical_or(current_whole_mask, test_mask).astype(np.uint8) * 255
+                        count_tries = 11
+                    else:
+                        count_tries += 1
+                
+                nodefect_image.paste(defect_image, (x_start - original_topleft_pos[1], y_start - original_topleft_pos[0]), defect_mask)
                 
             # save
-            nodefect_image_current.save(os.path.join(PATH_TO_SYNTHETIC, f'{nodefect_name}_{chosen_defect_type}_{i_sndp}.jpg'))
-            Image.fromarray(current_mask).convert('L').save(os.path.join(PATH_TO_SYNTHETIC_MASKS, f'{nodefect_name}_{chosen_defect_type}_{i_sndp}_mask.png'))   
-            print(f'{i_sndp + i_dt*samples_number_per_defect + 1}/{samples_number_per_defect*len(defect_types)}', end='\r')
+            nodefect_image.save(os.path.join(PATH_TO_SYNTHETIC, f'{nodefect_name}_{chosen_defect_type}_{i_samples_to_generate_per_defect}.jpg'))
+            Image.fromarray(current_whole_mask).convert('L').save(os.path.join(PATH_TO_SYNTHETIC_MASKS, f'{nodefect_name}_{chosen_defect_type}_{i_samples_to_generate_per_defect}_mask.png'))   
+            print(f'{i_samples_to_generate_per_defect + i_chosen_defect*samples_to_generate_per_defect + 1}/{samples_to_generate_per_defect*len(defect_types)}', end='\r')
             
     print('Done!')
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Generate synthetic images')
-    parser.add_argument('--tot_samples', type=int, help='Amount of synthetic images generated', required=True, dest='N_SAMPLES')    
+    parser.add_argument('--tot_samples', type=int, help='Amount of synthetic images generated', required=True, dest='N_SAMPLES', default=100)    
     args = parser.parse_args()
 
     os.makedirs(PATH_TO_SYNTHETIC, exist_ok=True)
     os.makedirs(PATH_TO_SYNTHETIC_MASKS, exist_ok=True)
     os.makedirs(PATH_TO_SYNTHETIC_DEFECTS_HARMONIZED, exist_ok=True)
     delete_old_files()
-    main(samples_number_per_defect=args.N_SAMPLES//5)
+    main(samples_to_generate_per_defect=args.N_SAMPLES//5)
     
